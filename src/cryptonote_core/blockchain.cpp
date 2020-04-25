367f31af73dad2f09b9c252f5f100a49f74d65a5
coinevo <coinevo12@gmail.com>
2020-04-25 08:24:00 +0200

    added checkpoints for hardfork_14

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5063,7 +5063,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "dd249599c48d0198cf982be12ce8c43efb78a8e0f7d687dca755343cd573f0c0";
+static const char expected_block_hashes_hash[] = "3e59ba86b76a296c8526301f3d5f549d33cd18f2595bd05dd3ec5d872bf53227";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
